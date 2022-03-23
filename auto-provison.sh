f161c1f648da2f2e3bcd13a6d0ec33ecfba79383
gitlawr <lawrleegle@gmail.com>
2022-03-23 09:11:44 +0800

    Create auto-provison.sh

---
+++ auto-provison.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ENDPOINT=https://ecs.aliyuncs.com
+REGION=cn-guangzhou
+ACCESS_KEY_ID=LTAI5tFc8mNs3zEnDYG7Yt6a
+ACCESS_KEY_Secret=sjSKtfG9l7QrPx8vBRryyykWfDQNpH
+
+function provision_ecs_machine {
+
+  return
+}
