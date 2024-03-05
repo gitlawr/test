55c4e9541f5ed02d432950e8a739ad72d280642d
gitlawr <lawrleegle@gmail.com>
2024-03-05 19:07:06 +0800

    update template

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 resource "kubernetes_deployment" "redis" {
   metadata {
-    name = "redis-${sha512(var.context.resource.id)}"
+    generate_name = "redis"
     namespace = var.context.runtime.kubernetes.namespace
     labels = {
       app = "redis"
@@ -44,7 +44,7 @@ resource "kubernetes_deployment" "redis" {
 
 resource "kubernetes_service" "redis" {
   metadata {
-    name = "redis-${sha512(var.context.resource.id)}"
+    generate_name = "redis"
     namespace = var.context.runtime.kubernetes.namespace
   }
   spec {
